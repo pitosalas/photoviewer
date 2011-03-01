7c8098c39c85e1d7a6f7bdc09609aa3b764333a7
Pito Salas <rps@salas.com>
2011-03-01 09:26:53 -0500

    Added  additional grid view. The grid view is so far, still invisible.

--- src/edu/brandeis/hellostepbystep/main.java
+++ src/edu/brandeis/hellostepbystep/main.java
@@ -9,5 +9,6 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        
+   }
 }
