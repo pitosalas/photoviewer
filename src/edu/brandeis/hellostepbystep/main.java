4c5757eec959ff14dd1a914920e6cf516cfc7d42
Pito Salas <rps@salas.com>
2011-03-01 11:00:51 -0500

    Added background colors, and sizes for various parts of the layout

--- src/edu/brandeis/hellostepbystep/main.java
+++ src/edu/brandeis/hellostepbystep/main.java
@@ -2,6 +2,8 @@ package edu.brandeis.hellostepbystep;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
@@ -10,5 +12,8 @@ public class main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+	    GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setBackgroundResource(R.drawable.bgr1);
+
    }
 }
