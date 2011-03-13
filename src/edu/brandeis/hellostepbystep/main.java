d481c75742bb3fd6d5cac6080e974eef0f2d5df9
Pito Salas <rps@salas.com>
2011-03-13 19:28:59 -0400

    add the static images and handling for them

--- src/edu/brandeis/hellostepbystep/main.java
+++ src/edu/brandeis/hellostepbystep/main.java
@@ -3,17 +3,24 @@ package edu.brandeis.hellostepbystep;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-	    GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setBackgroundResource(R.drawable.bgr1);
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
 
-   }
+	    GridView gridview = (GridView) findViewById(R.id.gridview);
+	    gridview.setAdapter(new ImageAdapter(this));
+
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            Toast.makeText(main.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	}
 }
