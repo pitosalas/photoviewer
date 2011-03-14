6151ec3252a6696bd3896bb9956cdeddfcbd4820
Pito Salas <rps@salas.com>
2011-03-13 21:33:05 -0400

    Add the menu with the two commands. They don't do anything yet.

--- src/edu/brandeis/hellostepbystep/main.java
+++ src/edu/brandeis/hellostepbystep/main.java
@@ -2,6 +2,8 @@ package edu.brandeis.hellostepbystep;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -9,7 +11,8 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 public class main extends Activity {
-    /** Called when the activity is first created. */
+
+/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.main);
@@ -22,5 +25,13 @@ public class main extends Activity {
 	            Toast.makeText(main.this, "" + position, Toast.LENGTH_SHORT).show();
 	        }
 	    });
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inf = getMenuInflater();
+		inf.inflate(R.menu.menu, menu);
+		return true;
 	}
 }
